dd5a8ca72649b5aa2412b5d19fc7a60c9c56a184
Park Hyunwoo <ez.amiryo@gmail.com>
2021-12-07 05:21:13 +0900

    Change domain

--- publishconf.py
+++ publishconf.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('.')
 from pelicanconf import *
 
-SITEURL = 'https://lqez.github.io/blog'
+SITEURL = 'https://lqez.dev/blog'
 
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
